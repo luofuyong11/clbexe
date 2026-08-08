bf42936da3758bf3c69156402b352e534090419c
DESKTOP-KG991LO\徐 <1335659534@qq.com>
2026-08-08 15:23:51 +0800

    安装器

--- scripts/build-nsis-installer.js
+++ scripts/build-nsis-installer.js
@@ -38,6 +38,7 @@ async function main() {
         perMachine: false,
         allowElevation: true,
         allowToChangeInstallationDirectory: true,
+        differentialPackage: false,
         artifactName: 'chonglaoban-Installer-${arch}.${ext}',
         installerIcon: existsSync(iconPath) ? iconPath : undefined,
         uninstallerIcon: existsSync(iconPath) ? iconPath : undefined,
